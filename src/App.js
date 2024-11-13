fa7912d02eea479d65106ce5d5b6c07afd101cd8
Dlayane-Mat <matjiladlayane@gmail.com>
2024-11-13 22:36:10 +0200

    added a netlify link

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ function App() {
           <input type="search" placeholder="Enter a city" />
           <input type="submit" value="Search" />
         </form>
+        <p>
+          This project was coded by{" "}
+          <a href="https://github.com/Dlayane-Mat/">Dlayane Matjila</a> and is
+          on{" "}
+          <a href="https://github.com/Dlayane-Mat/weather-react-app"> Github</a>{" "}
+          and hosted on{" "}
+          <a href="https://app.netlify.com/sites/superlative-kataifi-b85c70/overview">
+            Netlify
+          </a>
+        </p>
       </header>
-      <p>
-        This project was coded by{" "}
-        <a href="https://github.com/Dlayane-Mat/weather-react-app">
-          Dlayane Matjila
-        </a>
-      </p>
     </div>
   );
 }
