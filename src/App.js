bd70a5cfb5d627233fa49fefc1640946ea643d34
Dlayane-Mat <matjiladlayane@gmail.com>
2024-11-13 22:30:20 +0200

    added a form to my page and a github link

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <h1>React AJAX Search Engine</h1>
+        <form>
+          <input type="search" placeholder="Enter a city" />
+          <input type="submit" value="Search" />
+        </form>
       </header>
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/Dlayane-Mat/weather-react-app">
+          Dlayane Matjila
+        </a>
+      </p>
     </div>
   );
 }
